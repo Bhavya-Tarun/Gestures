c78b621a2e2a16b6f98643c371550f004f5ad647
Bhavya Tarun <80611224+Bhavya-Tarun@users.noreply.github.com>
2021-08-12 16:51:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: "png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach( '#camera');
+function takeSnapshot(){
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='"+data_url+"'/>"
+    });
+}
+    
+
